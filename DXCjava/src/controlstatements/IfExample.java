f159cb673d5d24fe001ea6f20533b01e7d047dc3
Lenovo <Lenovo@LenovoE4125>
2021-10-19 13:14:56 +0530

    Control Statements

---
+++ DXCjava/src/controlstatements/IfExample.java
@@ -0,0 +1,12 @@
+package controlstatements;
+
+public class IfExample {
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 30;
+		if(x-y > 10) {
+			System.out.println("x-y is greater than 10");
+		}
+	}
+
+}
---
+++ DXCjava/src/controlstatements/IfelseExample.java
@@ -0,0 +1,16 @@
+package controlstatements;
+
+public class IfelseExample {
+	public static void main(String[] args) {
+		int x = 12;
+		int y = 10;
+		if(x+y > 20) {
+			System.out.println("x+y is greater than 20");
+		}
+		else {
+			System.out.println("x+y is less than 20");
+		}
+		
+	}
+
+}
