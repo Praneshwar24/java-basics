65de298cc8c9f16f43547d93a4580af72095a1a2
Lenovo <Lenovo@LenovoE4125>
2021-10-25 11:28:32 +0530

    Threads

---
+++ DXCjava/src/threads/FeeThread.java
@@ -0,0 +1,21 @@
+package threads;
+
+/**
+ * person standing in the fee queue with my fee form
+ * @author Lenovo
+ *
+ */
+
+
+
+
+public class FeeThread extends Thread{
+	
+@Override
+	public void run() {
+	System.out.println("got fee noc");
+	}
+	
+	}
+
+
