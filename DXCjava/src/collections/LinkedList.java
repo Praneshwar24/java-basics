5df73bf53761ae11b4fa8e8da16e86ba6e52f1ed
Lenovo <Lenovo@LenovoE4125>
2021-10-26 16:38:37 +0530

    Linked List

---
+++ DXCjava/src/collections/LinkedList.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class LinkedList {
+	public static void main(String[] args) {
+		List<String> names = new java.util.LinkedList<String>();
+
+		names.add("Naruto");
+		names.add("Sasuke");
+		names.add("Kakashi");
+		names.add("Kakashi");
+
+		System.out.println(names);
+
+		Iterator<String> itr = names.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
