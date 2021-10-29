e52b67107473e5ec195dcb5f9284595c9229c06d
Lenovo <Lenovo@LenovoE4125>
2021-10-29 12:56:29 +0530

    Factory Design Pattern

---
+++ DXCjava/src/design/patterns/factory/DomesticPlan.java
@@ -0,0 +1,14 @@
+package design.patterns.factory;
+
+public class DomesticPlan extends Plan{
+
+	@Override
+	public void getRate() {
+		rate = 3.50;
+
+
+	}
+
+
+}
+//end of DomesticPlan class
---
+++ DXCjava/src/design/patterns/factory/InstitutionalPlan.java
@@ -0,0 +1,12 @@
+package design.patterns.factory;
+
+public class InstitutionalPlan extends Plan{
+
+	@Override
+	public void getRate() {
+		rate = 5.50;
+
+
+	}
+
+}
